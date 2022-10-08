026d18b52e4439bf461c484a2b4478dea59a8e8c
AXON <axonasif@gmail.com>
2022-10-08 10:38:14 +0000

    up

--- src/window.sh
+++ src/window.sh
@@ -1,3 +1,3 @@
 function tmux::create_window() {
-	tmux new-window -n "${WINDOW_NAME:-vs:${PWD##*/}}" -t "${TMUX_SESSION_NAME}" "$@";
+	tmux new-window -n "${WINDOW_NAME:"${PWD##*/}"}" -t "${TMUX_SESSION_NAME}" "$@";
 }
