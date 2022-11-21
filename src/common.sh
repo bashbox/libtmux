2863b38d223f9dabcae182264a139d892eadefc1
AXON <axonasif@gmail.com>
2022-11-22 00:02:27 +0600

    fix error out

--- src/common.sh
+++ src/common.sh
@@ -2,7 +2,7 @@ function tmux::show-option() {
 	local opt="$1";
 	local opt_val;
 
-	if opt_val="$(tmux start-server\; show-option -gv "$opt")" 2>/dev/null && test -n "${opt_val:-}"; then {
+	if opt_val="$(tmux start-server\; show-option -gv "$opt" 2>/dev/null)" && test -n "${opt_val:-}"; then {
 		printf '%s\n' "$opt_val";
 	} elif test -v DEFAULT_VALUE; then {
       printf '%s\n' "$DEFAULT_VALUE";
